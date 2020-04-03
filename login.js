2150e7fe706a62a13a13751eaf96d7baccfbc541
AritriBasu <59471237+AritriBasu@users.noreply.github.com>
2020-04-03 12:21:29 +0530

    updated javascript

--- login.js
+++ login.js
@@ -1,63 +1,25 @@
 
-
-var users=
-[
-    {
-        username:"aritri",
-        email:"aritri.basu2019@vitstudent.ac.in",
-        password:"aritri",
-    },
-
-    {
-        username:"kashish",
-        email:"kashish.mittal2019@vitstudent.ac.in",
-        password:"aritri",
-    },
-
-    {
-        username:"elio",
-        email:"elio.jordan2019@vitstudent.ac.in",
-        password:"aritri",
-    },
-    {
-        username:"anukriti",
-        email:"anukriti.verma2019@vitstudent.ac.in",
-        password:"aritri",
-    },
-    {
-        username:"kaustubh",
-        email:"kaustubh.shrivastava2019@vitstudent.ac.in",
-        password:"aritri",
-    }
-];
-
 function Login()
 {
     var username=document.getElementById("username").value;
     var email=document.getElementById("email").value;
     var password=document.getElementById("password").value; 
-    var u=0,e=0,p=0;
-    for(var i=0;i<users.length;i++)
+    var f=0;
+    for(let i=0;i<5;i++)
     {
-        if (username==users[i].username)
-        {
-            u=1;
-            if(email==users[i].email)
-            {
-                e=1;
-                if(password==users[i].password)
-                p=1;
-            }
-        }
+    fetch("http://localhost:3000/users")
+    .then(function(resp){return resp.json();})
+    .then(function(data){
+      if((username==data[i].username)&&(email==data[i].email)&&(password==data[i].password))
+         {
+         alert("Login Successful!");
+         f=1;
+         break;
+         } 
+      });
     }//for
-    if((u==1)&&(e==1)&&(p==1))
-    {
-        alert("Login successful!!");
-    }
-    else if(u==0)
-       alert("Enter correct username!");
-    else if(e==0)
-        alert("Enter correct email!");
-    else 
-        alert("Enter correct password!");
-}//func
+    if(f==1)   
+      alert("Login failed!Try Agin!");
+  }//func Login
+
+
