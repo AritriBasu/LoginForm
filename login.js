41cfe20ff9e4d365a843dfc2b53d8cfdd9fe74e0
AritriBasu <59471237+AritriBasu@users.noreply.github.com>
2020-04-08 14:51:25 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,25 +1,23 @@
-
 function Login()
 {
     var username=document.getElementById("username").value;
     var email=document.getElementById("email").value;
     var password=document.getElementById("password").value; 
     var f=0;
-    for(let i=0;i<5;i++)
-    {
-    fetch("http://f309063d.ngrok.io/users")
+    
+    fetch("http://1e9062ae.ngrok.io/users")
     .then(function(resp){return resp.json();})
     .then(function(data){
-      if((username==data[i].username)&&(email==data[i].email)&&(password==data[i].password))
+    for(let i=0;i<5;i++)
+    {
+      if((username==String(data[i].username))&&(email==String(data[i].email))&&(password==String(data[i].password)))
          {
-         alert("Login Successful!");
+         alert("Login succesful!");
          f=1;
          break;
          } 
-      });
-    }//for
-    if(f==0)   
+     }//for
+    });
+   if(f==0)   
       alert("Login failed!Try Agin!");
   }//func Login
-
-
